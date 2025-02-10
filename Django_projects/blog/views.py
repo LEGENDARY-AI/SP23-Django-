0b234b2e001dedeee1c3ee6fe22f376c9d715d94
LEGENDARY-AI <gamboandy444@gmail.com>
2025-02-10 00:42:30 +0000

    Deleted Django_projects from the repository

--- Django_projects/blog/views.py
+++
@@ -1,6 +0,0 @@
-from django.views.generic import ListView  
-from .models import Post  
-
-class PostListView(ListView):  
-    model = Post  
-    template_name = 'blog/post_list.html'  # Create this template
