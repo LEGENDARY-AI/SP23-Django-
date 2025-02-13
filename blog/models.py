226d4e61bd57882f5b1eb56c4c7fed5070a652a4
LEGENDARY-AI <gamboandy444@gmail.com>
2025-02-13 14:56:11 +0000

    Initial commit

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ class Post(models.Model):
     title = models.CharField(max_length=50)  
     content = models.TextField()  
     created_at = models.DateTimeField(auto_now_add=True)  
+    picture=models.ImageField( upload_to="images",default="images/default.png")
 
     def __str__(self):  
         return self.title
--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,4 @@ from .models import Post
 class PostListView(ListView):  
     model = Post  
     template_name = 'blog/post_list.html'  # Create this template
+    context_object_name = 'posts'
