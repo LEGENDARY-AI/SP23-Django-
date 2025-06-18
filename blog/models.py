c07aaf8a0dd64601d79d0a6e6fd27005e9da65f4
Andy Gambo <gamboandy444@gmail.com>
2025-06-18 16:30:56 +0000

    Limit post content length and update post list template
    Set a maximum length of 500 characters for the Post model's content field. Updated post_list.html to add a new section with style tips and adjusted CSS class usage.

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Post(models.Model):  
     name= models.CharField(max_length=100, default='Anonymous')  
     title = models.CharField(max_length=50)  
-    content = models.TextField()  
+    content = models.TextField(max_length=500)  
     created_at = models.DateTimeField(auto_now_add=True)  
     picture=models.ImageField( upload_to="images/",default="images/default.PNG")
 
